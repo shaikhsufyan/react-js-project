b95590e0bd85328d8fef4bd242c3f81ddd08995b
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-05-15 12:24:45 +0530

    StopWatch using React JS

---
+++ stopwatch/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './index.css';
+import Stopwatch from './Component/Stopwatch';
+
+ 
+function App() {
+  return (
+    <>
+       <Stopwatch/>
+    </>
+  );
+}
+
+export default App;
---
+++ stopwatch/src/Folder/Watch.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+// import ".index.css"
+
+const Watch = () => {
+    const [time, setTime] = useState({ hr:0, min:0, sec:0, milli:0 })
+    const [status, setStatus] = useState();
+
+    let updHr = time.hr;
+    let updMin = time.min;
+    let updSec = time.sec;
+    let updMilli = time.milli;
+
+    const start = () =>{
+        myFun();
+        setStatus(setInterval(myFun, 10))
+    }
+    const stop = () =>{
+       clearInterval(status)
+    }
+    const reset = () =>{
+        clearInterval(status);
+        setTime({ hr:0, min:0, sec:0, milli:0 })
+    }
+
+    const myFun = () =>{
+        if(updMilli === 100){
+            updMilli=0;
+            updSec++
+        }
+        if(updSec === 60){
+            updSec=0;
+            updMin++
+        }
+        if(updMin === 60){
+            updMin=0;
+            updHr++
+        }
+        updMilli++;
+        return setTime({ hr:updHr, min:updMin, sec:updSec, milli:updMilli})
+    }
+     
+    console.log(time);
+  return (
+    <>
+    
+      <div className="container">
+        <h1>{time.hr+" : " + time.min + " : " + time.sec + " : " + time.milli } </h1>
+        <div className='buttons'>
+            <button className='start' onClick={start}>Start</button>
+            <button className='stop' onClick={stop}>Stop</button>
+            <button className='reset' onClick={reset}>Reset</button>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Watch
