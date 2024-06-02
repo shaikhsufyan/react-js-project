9bbb9e1d10e617f54b836f3fa61d60a5ad01ed5e
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-06-02 08:58:10 +0530

    Font Icon

---
+++ greeting-website/src/Components/Icon.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Icon = () => {
+  return (
+    <>
+    <h1>How To Use Font Icon in React JS!!</h1>
+    <p>
+    <i class="fa-brands fa-linkedin"></i>
+    <i class="fa-solid fa-house"></i>
+    </p>
+    </>
+  )
+}
+
+export default Icon
