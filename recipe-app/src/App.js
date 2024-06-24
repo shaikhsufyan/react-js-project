70cd84195fce432d4c103cabc7a212bca7fd6b3a
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-06-24 14:53:26 +0530

    upd

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Food from './Components/Food';
-import { Route,Routes,BrowserRouter } from 'react-router-dom';
+import Mainpage from './Component/Mainpage';
+import { Route,Routes } from 'react-router-dom';
+import Mealinfo from './Component/Mealinfo';
+import Food from "./Components/Food"
 import Recipe from './Components/Recipe';
+ 
 
-function App() {
+function App() { 
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Food/>}/>
-      <Route path='/:meal' element={<Recipe/>}/>
-
-    </Routes>
-      
-    </BrowserRouter>
      
+    // <Mainpage/>
+     
+     <Routes>
+      <Route path='/' element={<Food/>}/>
+      <Route path='/:mealid' element={<Recipe/>}/>
+     </Routes>
+      
   );
 }
-
+ 
 export default App;
