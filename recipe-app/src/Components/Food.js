3def2b885d39a71c641900859de94271ace29b0a
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-06-27 21:52:25 +0530

    recipe app

--- recipe-app/src/Components/Food.js
+++ recipe-app/src/Components/Food.js
@@ -5,31 +5,39 @@ import Cards from './Cards';
 
 const Food = () => {
 
-    const [search, setSearch] = useState();
+    const [search, setSearch] = useState("");
     const [data,setData] = useState()
-    // const [recipeData, setRecipeData] = useState();
-     
+    const [msg, setMsg] = useState("Search and Get Recipe's")
 
     const handleInput = (event) =>{
         setSearch(event.target.value);
     }
 
     const myFun = async () =>{
+         
+        if(search === ""){
+            setMsg("Please Enter Something")
+        }else{
         const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
         const jsonData = await get.json();
         console.log(jsonData.meals);
         setData(jsonData.meals)
+        setMsg("Your Search Result's")
+         
+        }
+         
     }
      
     
   return (
     <>
-     
+         <h1 className='head'>FOOD RECIPE APP</h1>
         <div className='container'>
             <div className='searchBar'>
                 <input placeholder='Search Meals' type='text' onChange={handleInput}/>
                 <button onClick={myFun}>Search</button>
             </div>
+            <h2 className='msg'>{msg}</h2>
              <div>
              <Cards detail={data}/>
              </div>
