a70693d73994ab5e0631ea430d28b3e625015d64
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-06-28 19:55:25 +0530

    upd

--- image-search-app/src/App.js
+++ image-search-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Searchapp from './Components/Searchapp';
+import Imageapp from './Components/Imageapp';
+ 
 
 function App() {
   return (
      <>
-      <Searchapp/>
+       <Imageapp/>
+        
      </>
   );
 }
