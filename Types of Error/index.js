4b22ce0283aa4bcc063dd7c5df57a21d296be2e1
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-07-24 09:21:00 +0530

    Errors

--- Types of Error/index.js
+++ Types of Error/index.js
@@ -1,16 +1,9 @@
 //  FRONTEND INTERVIEW QUESTION
 // Reference() Error vs Type() vs Syntax()
  
-const b = 10;
-b=100;
- 
- 
-console.log(b);
-
-
-
-
-
+ const myFun = (event) =>{
+    console.log(event.target.value);
+ }
 
 
 
