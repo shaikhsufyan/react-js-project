318d60e51e471e09b37528eb7a20165ca9bf9b2f
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-08-04 20:52:12 +0530

    registration Form ReactJS

---
+++ Registration Form Reactjs/registration-form/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './Component/Signup';
+
+function App() {
+  return (
+     <Signup/>
+  );
+}
+
+export default App;
---
+++ Registration Form Reactjs/registration-form/src/Component/Signup.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const Signup = () => {
+    const [data, setData] = useState()
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
