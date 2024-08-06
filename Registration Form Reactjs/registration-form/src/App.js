e4b7bcb96677d1f471d858bf770de04d0f02f387
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-08-06 09:26:22 +0530

    Signup Form

--- Registration Form Reactjs/registration-form/src/App.js
+++ Registration Form Reactjs/registration-form/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './Component/Signup';
-
+ 
 function App() {
   return (
-     <Signup/>
+    <>
+    <Signup/>
+    </>
   );
 }
 
