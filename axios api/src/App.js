51c2ecb9968192b548d0cdf486692fc0291a9cc3
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-08-16 11:34:35 +0530

    Axios API

---
+++ axios api/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Axios from './Axios';
+
+function App() {
+  return (
+     <Axios/>
+  );
+}
+
+export default App;
---
+++ axios api/src/Axios.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Axios = () => {
+    const [store, setStore] = useState()
+    
+    // const getData = () =>{
+    //     axios.get("https://jsonplaceholder.typicode.com/users")
+    //     .then((res)=>{
+    //         console.log(res.data);
+    //         setStore(res.data)
+            
+    //     }).catch((err)=>{
+    //         console.log(err);
+            
+    //     })
+    // }
+
+    const getData = async() =>{
+        try{
+            const respone = await axios.get("https://jsonplaceholder.typicode.com/user");
+            console.log(respone);
+        }catch(err){
+            console.log(err);
+            
+        }
+        
+        
+         
+    }
+
+    useEffect(()=>{
+        getData()
+    },[])
+   
+
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default Axios
