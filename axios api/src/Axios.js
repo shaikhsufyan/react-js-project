bbe17a7448542a1dadf3f0473d57e952e3bf6481
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-08-26 23:31:42 +0530

    axios

--- axios api/src/Axios.js
+++ axios api/src/Axios.js
@@ -4,30 +4,27 @@ import axios from 'axios'
 const Axios = () => {
     const [store, setStore] = useState()
     
-    // const getData = () =>{
-    //     axios.get("https://jsonplaceholder.typicode.com/users")
-    //     .then((res)=>{
-    //         console.log(res.data);
-    //         setStore(res.data)
+    const getData = () =>{
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((res)=>{
+            console.log(res.data);
+            setStore(res.data)
             
-    //     }).catch((err)=>{
-    //         console.log(err);
-            
-    //     })
-    // }
-
-    const getData = async() =>{
-        try{
-            const respone = await axios.get("https://jsonplaceholder.typicode.com/user");
-            console.log(respone);
-        }catch(err){
+        }).catch((err)=>{
             console.log(err);
             
-        }
-        
-        
-         
+        })
     }
+
+    // const getData = async() =>{
+    //     try{
+    //         const respone = await axios.get("https://jsonplaceholder.typicode.com/user");
+    //         console.log(respone);
+    //     }catch(err){
+    //         console.log(err);
+            
+    //     }
+    // }
 
     useEffect(()=>{
         getData()
