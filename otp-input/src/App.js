48d660419d6f210d90114b3855780333af603288
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-09-08 19:48:55 +0530

    OTP Input React

--- otp-input/src/App.js
+++ otp-input/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Otp from './Component/Otp';
+ 
 
 function App() {
   return (
-    <div>
+    <>
       <Otp/>
-    </div>
+    </>
   );
 }
 
