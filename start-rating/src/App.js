b172c2db0e983efd7a9070c51d537cd20ffbe803
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-09-17 02:29:16 +0530

    Star Rating ReactJS

---
+++ start-rating/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Rating from './Components/Rating';
+
+ 
+function App() {
+  return (
+      <Rating/>
+  );
+}
+
+export default App;
---
+++ start-rating/src/Components/Rating.js
@@ -0,0 +1,29 @@
+ import React, { useState } from 'react'
+ 
+ const Rating = () => {
+    const [rating, setRating] = useState();
+    const [hover, setHover] = useState(0)
+    console.log(rating);
+    
+    let arr = new Array(5).fill(0);
+    
+    
+   return (
+     <div className='container'>
+        {
+            arr.map((curValue, index)=>{
+                return(
+                    <span
+                    className={hover == 0 && index<rating || index<hover ?"colored":"unColor"}
+                    onClick={()=>setRating(index+1)}
+                    onMouseEnter={()=>setHover(index+1)}
+                    onMouseLeave={()=>setHover(0)}
+                     key={index}>&#9733;</span>
+                )
+            })
+        }
+     </div>
+   )
+ }
+ 
+ export default Rating
