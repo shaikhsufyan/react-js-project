af6d1ecb92000f7bbd492f4dbffe335f4e394e04
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-10-17 09:17:10 +0530

    FullStack Gym Web

--- fullstackgym/Backend/src/database/db.js
+++ fullstackgym/Backend/src/database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://codewithsk99:UqYxThUqUSwVovN7@cluster0.ywhxpk0.mongodb.net/GymData")
+mongoose.connect("mongodb://localhost:27017/user")
  
 .then(()=>{
     console.log("DB Connect Succesfully");
