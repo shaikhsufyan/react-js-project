2cb568b12393ce2f3023e529190881820869ce14
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-11-30 10:51:09 +0530

    React Icons

--- react icons/react-icons/src/App.js
+++ react icons/react-icons/src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Profile from './Profile';
-import w1 from "./images/wo1.jpg"
-import w2 from "./images/wo2.jpg"
-import w3 from "./images/wo3.jpg"
+import MyComponent from './MyComponent';
 
 function App() {
   return (
     <>
-     <div className='user'>
-     <Profile img={w1}/>
-    <Profile img={w2}/>
-    <Profile img={w3}/>
-     </div>
+         <MyComponent/>
     </>
 
   );
